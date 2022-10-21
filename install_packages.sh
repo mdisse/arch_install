3a75662793f80eb379b34d158ea70194d8b38a42
Max Disselnmeyer <max.disselnmeyer@isst.fraunhofer.de>
2022-10-21 10:15:10 +0200

    Add if statement to yay installation

--- install_packages.sh
+++ install_packages.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# First install yay 
-sudo pacman -Syu --noconfirm git base-devel
-cd /opt
-sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R $USER:$USER ./yay-git
-cd yay-git
-makepkg -si --noconfirm
+# Install essentials
+sudo pacman -Syu --noconfirm --needed \
+	git \
+	base-devel \
+	vim 
+
+# First install yay if not installed 
+if [ ! -d "/opt/yay-git" ]; then 
+	cd /opt
+	sudo git clone https://aur.archlinux.org/yay-git.git
+	sudo chown -R $USER:$USER ./yay-git
+	cd yay-git
+	makepkg -si --noconfirm
+fi 
