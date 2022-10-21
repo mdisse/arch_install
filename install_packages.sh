5dc1bde6f2785ce6cf5848b74000460b7ef1c3ea
Max Disselnmeyer <max.disselnmeyer@isst.fraunhofer.de>
2022-10-21 10:05:59 +0200

    Add yay installation

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # First install yay 
+sudo pacman -Syu --noconfirm git base-devel
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git
+makepkg -si --noconfirm
