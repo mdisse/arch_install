c9f2bf1a359ac8870d92852f80a5c6260eb4f095
Max Disselnmeyer <max.disselnmeyer@isst.fraunhofer.de>
2022-10-21 12:39:37 +0200

    Add autosuggestions

--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@ export ZSH="/home/$USER/.oh-my-zsh"
 ZSH_THEME="agnoster"
 unsetopt correct 
 
-plugins=(git fzf)
+plugins=(git fzf zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
--- install_packages.sh
+++ install_packages.sh
@@ -24,4 +24,5 @@ vim +'PlugInstall --sync' +qa
 
 # Setup zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp .zshrc ~/.zshrc
